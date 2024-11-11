3e8c2d34b753f18932b15d9d62f411bb832dd479
Pablofl0 <pfernandezlamas15@gmail.com>
2024-11-11 10:59:02 +0100

    t02_02_exer10: completo

--- T2/t02_07_Fernández_Lamas/exer10.py
+++ T2/t02_07_Fernández_Lamas/exer10.py
@@ -14,10 +14,33 @@ ordenar unha lista de números de menor a maior.
 
 __author__ = "Pablo Fernández Lamas"
 
+#Def validacion del número pedido.
+def validacion(número_pedir):
+    """Función que valida el número pedido.
 
+    Args:
+        número_pedir (int): número pedido.
+
+    Raises:
+        ValueError: si el tipo del número es entero.
+    """
+    if type(número_pedir) is not int:
+        raise ValueError
+    else: 
+        pass
+
+#Def ordenar la lista.
 def ordenar(lista):
-    i=1
-    while True:
+    """Función que ordena la lista de números entregados por el usuario
+
+    Args:
+        lista (list): lista de números sin ordenar.
+
+    Returns:
+        list: lista con los números ordenados.
+    """
+    i=0
+    while i<(len(lista)-1):
         lista_ordenada=lista.sort()
         if lista_ordenada == lista:
             break
@@ -27,16 +50,29 @@ def ordenar(lista):
             elif lista[i] < lista[i+1]:
                 i+=1
             elif lista[i] > lista[i+1]:
-                i+=1
+                reemplazo=lista[i]
                 lista[i]=lista[i+1]
-                lista[i+1]=lista[i]
+                lista[i+1]=reemplazo
+                i+=1
+    return lista
 
 
 
 #Inicializando variable.
 lista=[]
 
-
+#Petición de números.
 while True:
-    número_pedir=int(input("Introduzca un número: "))
-    lista.append(número_pedir)
+    número_pedir=(str(input("Introduzca un número: ")))
+    if número_pedir=="listo":
+        break
+    else:
+        try:
+            numero_int=int(número_pedir	)
+            validacion(numero_int)
+            lista.append((numero_int))
+        except ValueError:
+            print("Error. Entrada no válida.")
+
+#Muestra de la lista de números ordenada.
+print(ordenar(lista))
