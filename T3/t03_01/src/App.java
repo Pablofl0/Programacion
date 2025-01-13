2d92b8aa47d7eec3ac0431a97b1104c12c9661c9
Pablofl0 <pfernandezlamas15@gmail.com>
2025-01-13 11:33:56 +0100

    t03_01_exer1: completado

---
+++ T3/t03_01/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Integer edad = 25;
+        
+
+        edad += 10;
+        System.out.println(edad);
+    }
+}
