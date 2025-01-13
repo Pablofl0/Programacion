0042a69c5abb09cfcb0e810a50e38316ca27add3
Pablofl0 <pfernandezlamas15@gmail.com>
2025-01-13 11:56:09 +0100

    t03_01_exer3: completado

--- T3/t03_01/exer3/src/App.java
+++ T3/t03_01/exer3/src/App.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Double precioManzana = 3.5;
+        Double precioNaranja = 1.6;
+
+        Scanner scanner = new Scanner(System.in);
+
+        double kilosMan = scanner.nextDouble();
+        double kilosNar = scanner.nextDouble();
+
+
+
+
+        System.out.println((precioManzana * kilosMan) + (precioNaranja * kilosNar));
     }
 }
