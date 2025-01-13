7c5e4d23f7787ddb187d17445ba9967e0b55ed52
Pablofl0 <pfernandezlamas15@gmail.com>
2025-01-13 13:40:53 +0100

    t03_01_exer8: empezando

---
+++ T3/t03_01/exer8/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        while (true) {
+            if (i*i <= numero) {
+                i++;
+            }
+            else {
+                
+            }
+        }
+        System.out.println("Hello, World!");
+    }
+}
