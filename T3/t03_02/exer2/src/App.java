5408bd3206e313acbc03f3d31fef7ce1e3e2e926
Pablofl0 <pfernandezlamas15@gmail.com>
2025-01-14 21:21:52 +0100

    t03_02_exer2: completado

--- T3/t03_02/exer2/src/App.java
+++ T3/t03_02/exer2/src/App.java
@@ -6,16 +6,36 @@ public class App {
 
         int lista [] = {3,5,7,2,9};
 
-        Integer indice = scanner.nextInt();
-
-        if (indice<0 || indice>=lista.length) {
-            System.out.println("Error.");
-        }
-
         
 
+        while (lista.length>0) {
+            Integer indice = scanner.nextInt();
 
-        System.out.println("Hello, World!");
+
+            if (indice<0 || indice>=lista.length) {
+                System.out.println("Error.");
+                continue;
+            }
+            
+            int longitud = (lista.length-1);
+            int listaNueva [] = new int[longitud];
+            int j = 0;
+
+            for (int i = 0; i < lista.length; i++) {
+                
+                if (i!= indice){
+                    listaNueva[j] = lista[i];
+                    j++;
+                }
+                
+                
+            }
+            System.out.println(lista[indice]);
+
+
+            lista = listaNueva;
+        }
+        
 
 
         scanner.close();
