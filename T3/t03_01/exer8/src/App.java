27a24b374ce23c8dda0bcd15ec5c05893bbe86ba
Pablofl0 <pfernandezlamas15@gmail.com>
2025-01-14 10:08:53 +0100

    t03_01_exer8: completado

--- T3/t03_01/exer8/src/App.java
+++ T3/t03_01/exer8/src/App.java
@@ -1,13 +1,27 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        Integer i = 0;
+        Integer raiz;
+        Integer numero = scanner.nextInt();
+
+
         while (true) {
             if (i*i <= numero) {
                 i++;
             }
             else {
-                
+                raiz = i-1;
+                break;
             }
         }
-        System.out.println("Hello, World!");
+
+        System.out.println(raiz);
+
+        scanner.close();
+
     }
 }
