08a1a272e87ae6c18e4c3d57cf41fffba3aa216f
Pablofl0 <pfernandezlamas15@gmail.com>
2025-01-18 13:33:57 +0100

    t03_03_exer3: completado 2

--- T3/Tareas/t03_03_Fernandez_Lamas/exer2/src/App.java
+++ T3/Tareas/t03_03_Fernandez_Lamas/exer2/src/App.java
@@ -4,29 +4,38 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner Scanner = new Scanner(System.in);
 
-        System.out.println("Número de pasajeros en la parte superior: ");
-        int arriba = Scanner.nextInt();
-        System.out.println("Número de pasajeros en la parte inferior: ");
-        int abajo = Scanner.nextInt();
+        boolean seguir = true;
+        while (seguir) {
+            System.out.println("Número de pasajeros en la parte superior: ");
+            int arriba = Scanner.nextInt();
+            System.out.println("Número de pasajeros en la parte inferior: ");
+            int abajo = Scanner.nextInt();
 
-        int datos[][] = new int[4][4];
+            int datos[][] = new int[4][4];
 
-        int limite = 0;
-        int variable = 1;
-        while (limite<datos.length) {
-            for (int i = 0; i < datos.length; i++) {
-                for (int j = 0; j < datos.length; j++) {
-                    if (i+j == limite) {
-                        datos[i][j] = variable;
-                        variable++;
+            int limite = 0;
+            int variable = 1;
+            while (limite < datos.length) {
+                for (int i = 0; i < datos.length; i++) {
+                    for (int j = 0; j < datos.length; j++) {
+                        if (i + j == limite) {
+                            datos[i][j] = variable;
+                            variable++;
+                        }
                     }
                 }
+                limite++;
             }
-            limite++;
+
+            if (datos[arriba][abajo] == 0) {
+                System.out.println("Número de pasajeros incorrectos.");
+            }
+            else{
+                System.out.println("Código:" + datos[arriba][abajo]);
+                seguir = false;
+            }
         }
 
-        System.out.println("Código:" + datos[arriba][abajo]);
-   
         Scanner.close();
     }
 }
