c29301156301b347d67aa1aae96289b2ce7f5fac
Pablofl0 <pfernandezlamas15@gmail.com>
2025-02-04 10:02:45 +0100

    t04_01_exer9: entregable

--- T3/Tareas/t04_01_Fernandez_Lamas/exer9/src/App.java
+++ T3/Tareas/t04_01_Fernandez_Lamas/exer9/src/App.java
@@ -1,5 +1,35 @@
+import Concesionario.Vehiculo;
+import dawutils.TipoCombustible;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        System.out.println(Vehiculo.getNúmeroObjetos()); // 0
+        
+        Vehiculo leon = new Vehiculo("1234CGF", "SEAT", "León");
+        System.out.println(Vehiculo.getNúmeroObjetos()); // 1
+        Vehiculo c3 = new Vehiculo("6894DEF", "Citroen", "C3", TipoCombustible.ELÉCTRICO);
+        System.out.println(Vehiculo.getNúmeroObjetos()); // 2
+
+        System.out.println(leon.getMatricula() + " " + leon.getTipoCombustible()); // 1234CGF GASOLINA
+        System.out.println(c3.getMatricula() + " " + c3.getTipoCombustible()); // 0000XXX ELECTRICO
+
+        leon.acelerarVar(20);
+        leon.acelerar();
+        System.out.println(leon.getVelocidad()); // 30.0
+
+        leon.acelerarVar(100);
+        System.out.println(leon.getVelocidad()); // 120.0
+
+        leon.decelerar();
+        leon.decelerarVar(200);
+        System.out.println(leon.getVelocidad()); // 0.0
+
+        Vehiculo.setVelocidadMax(130);
+
+        leon.acelerarVar(125);
+        System.out.println(leon.getVelocidad()); // 125.0
+
     }
+
 }
--- T3/Tareas/t04_01_Fernandez_Lamas/exer9/src/Concesionario/Vehiculo.java
+++ T3/Tareas/t04_01_Fernandez_Lamas/exer9/src/Concesionario/Vehiculo.java
@@ -4,31 +4,85 @@ import dawutils.TipoCombustible;
 
 public class Vehiculo {
     private static int númeroObjetos = 0;
-    private static double velocidadMax = 120;
+    private static int velocidadMax = 120;
 
     private String matricula;
     private String marca;
     private String modelo;
-    private double velocidad;
+    private int velocidad;
     private TipoCombustible tipoCombustible;
 
 
-    
-    public Vehiculo(String matricula, String marca, String modelo, double velocidad, TipoCombustible tipoCombustible) {
+    /**
+     * 
+     * @param matricula matrícula del coche
+     * @param marca marca del coche
+     * @param modelo modelo del coche
+     * @param velocidad velocidad del coche
+     * @param tipoCombustible tipo de combustible
+     */
+    public Vehiculo(String matricula, String marca, String modelo) {
         if (validarMatricula(matricula)) {
-            this.matricula = matricula;
+            this.setMatricula(matricula);
         }
         else{
-            this.matricula = "0000XXX";
+            this.setMatricula("0000XXX");
         }
-        this.marca = marca;
-        this.modelo = modelo;
-        this.velocidad = velocidad;
-        if (tipoCombustible == TipoCombustible.DIÉSEL) {
-            this.tipoCombustible = 
-        }
-        this.tipoCombustible = tipoCombustible;
+        this.setMarca(marca);
+        this.setModelo(modelo);
+        this.setVelocidad(0);
+        this.setTipoCombustible(TipoCombustible.GASOLINA);
+        Vehiculo.sumNumero();
     }
+
+    /**
+     * 
+     * @param matricula matrícula del coche
+     * @param marca marca del coche
+     * @param modelo modelo del coche
+     * @param tipoCombustible tipo de combustible
+     */
+    public Vehiculo(String matricula, String marca, String modelo, TipoCombustible tipoCombustible) {
+        if (validarMatricula(matricula)) {
+            this.setMatricula(matricula);
+        }
+        else{
+            this.setMatricula("0000XXX");
+        }
+        this.setMarca(marca);
+        this.setModelo(modelo);
+        this.setTipoCombustible(tipoCombustible);
+        Vehiculo.sumNumero();
+    }
+
+
+
+    private void modificar(int var){
+        this.velocidad = velocidad + var;
+        if (velocidad < 0) {
+            this.velocidad = 0;
+        }
+        else if (velocidad > velocidadMax) {
+            this.velocidad = 120;
+        }
+    }
+    public void acelerar(){
+        int var = 10;
+        this.modificar(var);;
+    }
+    public void acelerarVar(int var){
+        this.modificar(var);;
+    }
+    public void decelerar(){
+        int var = -10;
+        this.modificar(var);;
+    }
+    public void decelerarVar(int var){
+        this.modificar(-var);;
+    }
+
+
+
 
     public static int getNúmeroObjetos() {
         return númeroObjetos;
@@ -38,11 +92,15 @@ public class Vehiculo {
         Vehiculo.númeroObjetos = númeroObjetos;
     }
 
-    public static double getVelocidadMax() {
+    public static void sumNumero(){
+        Vehiculo.númeroObjetos += 1;
+    }
+
+    public static int getVelocidadMax() {
         return velocidadMax;
     }
 
-    public static void setVelocidadMax(double velocidadMax) {
+    public static void setVelocidadMax(int velocidadMax) {
         Vehiculo.velocidadMax = velocidadMax;
     }
 
@@ -51,9 +109,7 @@ public class Vehiculo {
     }
 
     public void setMatricula(String matricula) {
-        if (validarMatricula(matricula)) {
             this.matricula = matricula;   
-        }
     }
 
     public String getMarca() {
@@ -72,11 +128,11 @@ public class Vehiculo {
         this.modelo = modelo;
     }
 
-    public double getVelocidad() {
+    public int getVelocidad() {
         return velocidad;
     }
 
-    public void setVelocidad(double velocidad) {
+    public void setVelocidad(int velocidad) {
         this.velocidad = velocidad;
     }
 
@@ -95,11 +151,11 @@ public class Vehiculo {
 
         // Cogemos los números y las letras.
         String numeros = matricula.substring(0, 4);
-        String letras = matricula.substring(4, 6);
+        String letras = matricula.substring(4, 7);
 
         // Comprobar que son 4 números.
         for (int i = 0; i < numeros.length(); i++) {
-            if ((int) numeros.charAt(i) < 0 || (int) numeros.charAt(i) > 9) {
+            if ((int)numeros.charAt(i) < 0 || (int)numeros.charAt(i) > 9) {
                 return false;
             }
         }
