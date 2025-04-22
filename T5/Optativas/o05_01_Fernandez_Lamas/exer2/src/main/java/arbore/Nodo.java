f3f10823a4c696b4c15c19bbeb04e7c1d73f5935
Pablofl0 <pfernandezlamas15@gmail.com>
2025-04-22 13:00:42 +0200

    Arbol

--- T5/Optativas/o05_01_Fernandez_Lamas/exer2/src/main/java/arbore/Nodo.java
+++ T5/Optativas/o05_01_Fernandez_Lamas/exer2/src/main/java/arbore/Nodo.java
@@ -4,4 +4,14 @@ public class Nodo<T> {
     private Nodo<T> subIzq;
     private Nodo<T> subDer;
 
+    
+    public Nodo<T> getSubIzq() {
+        return subIzq;
+    }
+    public Nodo<T> getSubDer() {
+        return subDer;
+    }
+
+
+    
 }
