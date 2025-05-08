9b86a87c5418962013ed53ac13e13537993873cb
Pablofl0 <pfernandezlamas15@gmail.com>
2025-05-08 20:54:48 +0200

    t06_02_exer1

--- T6/Tareas/t06_02_Fernandez_Lamas/exer1/src/Modelo/Cliente.java
+++ T6/Tareas/t06_02_Fernandez_Lamas/exer1/src/Modelo/Cliente.java
@@ -1,100 +1,41 @@
 package Modelo;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import Excepciones.ExcepcionEmailInvalido;
 import Excepciones.ExcepcionGeneral;
+import java.util.ArrayList;
 
 public class Cliente extends Usuario {
-    private Libro libroPrestado;
-    private LocalDate fechaPrestamo;
-    private LocalDate fechaLimiteDevolucion;
-    private LocalDate fechaDevolucion;
-    private LocalDate fechaFinDeSancion;
-    private boolean libroDevuelto;
-    private boolean sancionado;
 
-    private static DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private ArrayList<Prestamo> prestamos;
+    private final int CERO = 0;
+    private final int UNO = 1;
+    private final int DOS = 2;
 
     public Cliente(String nombreUsuario, String contrasenhaUsuario, TipoUsuario tipoUsuario, String nombre,
             String apellido1, String apellido2, String dni, String correo)
             throws ExcepcionGeneral, ExcepcionEmailInvalido {
-        super(nombreUsuario, contrasenhaUsuario, TipoUsuario.CLIENTE, nombreUsuario, contrasenhaUsuario,
-                contrasenhaUsuario, nombreUsuario, contrasenhaUsuario);
-        this.setSancionado(false);
-    }
-
-    public LocalDate cambioAFechaNoDomingo(LocalDate fecha) {
-        LocalDate newFecha = null;
-        if (fecha.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
-            newFecha = fecha.plusDays(1);
-        }
-        return newFecha;
-    }
-
-    public Libro getLibroPrestado() {
-        return libroPrestado;
-    }
-
-    public void setLibroPrestado(Libro libroPrestado) {
-        this.libroPrestado = libroPrestado;
-    }
-
-    public LocalDate getFechaPrestamo() {
-        return fechaPrestamo;
-    }
-
-    public void setFechaPrestamo(String fechaPrestamo) {
-        this.fechaPrestamo = LocalDate.parse(fechaPrestamo, formato);
-        this.setLibroDevuelto(false);
-    }
-
-    public LocalDate getFechaLimiteDevolucion() {
-        return fechaLimiteDevolucion;
-    }
-
-    public void setFechaLimiteDevolucion() {
-        this.fechaLimiteDevolucion = cambioAFechaNoDomingo(this.fechaPrestamo.plusDays(15));
-    }
-
-    public LocalDate getFechaDevolucion() {
-        return fechaDevolucion;
-    }
-
-    public void setFechaDevolucion(String fechaDevolucion) {
-        this.fechaDevolucion = LocalDate.parse(fechaDevolucion, formato);
-        if (this.fechaDevolucion.isAfter(fechaLimiteDevolucion)) {
-            this.setSancionado(true);
-            this.setFechaFinDeSancion();
-        }
-        this.setLibroDevuelto(true);
-        this.setLibroPrestado(null);
-    }
-
-    public LocalDate getFechaFinDeSancion() {
-        return fechaFinDeSancion;
-    }
-
-    public void setFechaFinDeSancion() {
-        this.fechaFinDeSancion = cambioAFechaNoDomingo(this.fechaPrestamo.plusYears(1));
-    }
-
-    public boolean isLibroDevuelto() {
-        return libroDevuelto;
-    }
-
-    public void setLibroDevuelto(boolean libroDevuelto) {
-        this.libroDevuelto = libroDevuelto;
+        super(nombreUsuario, contrasenhaUsuario, TipoUsuario.CLIENTE, nombre, apellido1, apellido2, dni, correo);
     }
 
     public boolean isSancionado() {
-        return sancionado;
+        int suma = CERO;
+        for (Prestamo prestamo : prestamos) {
+            if (prestamo.isFalta()) {
+                suma += UNO;
+                if (suma == DOS) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
-    public void setSancionado(boolean sancionado) {
-        this.sancionado = sancionado;
+    public boolean tieneLibrosPrestados() {
+        for (Prestamo prestamo : prestamos) {
+            if (prestamo.isEnPrestamo()) {
+                return true;
+            }
+        }
+        return false;
     }
-
 }
---
+++ T6/Tareas/t06_02_Fernandez_Lamas/exer1/src/Modelo/Prestamo.java
@@ -0,0 +1,79 @@
+package Modelo;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class Prestamo {
+
+    private Libro libroPrestado;
+    private LocalDate fechaPrestamo;
+    private LocalDate fechaLimiteDevolucion;
+    private LocalDate fechaDevolucion;
+    private boolean enPrestamo;
+
+    private static DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public Prestamo(Libro libroPrestado, String fechaPrestamo) {
+        this.setLibroPrestado(libroPrestado);
+        this.setFechaPrestamo(fechaPrestamo);
+        this.setEnPrestamo(true);
+    }
+
+    public LocalDate cambioAFechaNoDomingo(LocalDate fecha) {
+        LocalDate newFecha = null;
+        if (fecha.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
+            newFecha = fecha.plusDays(1);
+        }
+        return newFecha;
+    }
+
+    public boolean isFalta() {
+        if (this.fechaDevolucion.isAfter(this.fechaLimiteDevolucion) && ChronoUnit.YEARS.between(this.fechaPrestamo, LocalDate.now()) < 1) {
+            return true;
+        }
+        return false;
+    }
+
+    public Libro getLibroPrestado() {
+        return libroPrestado;
+    }
+
+    public void setLibroPrestado(Libro libroPrestado) {
+        this.libroPrestado = libroPrestado;
+    }
+
+    public LocalDate getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(String fechaPrestamo) {
+        this.fechaPrestamo = LocalDate.parse(fechaPrestamo, formato);
+    }
+
+    public LocalDate getFechaLimiteDevolucion() {
+        return fechaLimiteDevolucion;
+    }
+
+    public void setFechaLimiteDevolucion() {
+        this.fechaLimiteDevolucion = cambioAFechaNoDomingo(this.fechaPrestamo.plusDays(15));
+    }
+
+    public LocalDate getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(String fechaDevolucion) {
+        this.fechaDevolucion = LocalDate.parse(fechaDevolucion, formato);
+        this.setEnPrestamo(false);
+    }
+
+    public boolean isEnPrestamo() {
+        return enPrestamo;
+    }
+
+    public void setEnPrestamo(boolean enPrestamo) {
+        this.enPrestamo = enPrestamo;
+    }
+}
